11cba2767f1da9365aac33c0235db7f0e5faa84d
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-02-08 18:53:59 +0900

    68 moment.js

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -22,6 +22,7 @@
 <script>
 import NewMessage from '@/components/NewMessage'
 import db from '@/firebase/init'
+import moment from 'moment'
 
 export default {
   name: 'Chat',
@@ -46,7 +47,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format('lll')
           })
         }
       })
@@ -65,7 +66,7 @@ export default {
 }
 .chat .time {
   display: block;
-  font-size: 1.4em;
+  font-size: 0.8em;
 }
 </style>
 
